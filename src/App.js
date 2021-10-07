01c29bfb4fb09e136f0bc5d8fbbf0af2e8b4efeb
emilioMarchi <em.98.nmynm@gmail.com>
2021-10-06 22:55:45 -0300

    nuevo commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Nav from './components/Nav';
 import ItemsContainer from './components/ItemsContainer';
 
-function App() {
+import axios from 'axios';
+
+const App = () => {
+    const [data, setData] = React.useState({});
+
+    useEffect(()=>{
+      axios(
+        'https://http.cat/[status_code]'
+      ).then((res) => {console.log(res)});
+    },[]);
+    
+
     return (
       <div>
         <Nav />
         <ItemsContainer />
       </div>
     );
-  }
+}
 
 export default App;
